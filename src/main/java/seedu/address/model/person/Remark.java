d50205d20c67f20cae05d6729f44418c677d28c7
delonlee01 <delonlee2001@hotmail.com>
2025-02-20 21:14:10 +0800

    Add Remark class

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,39 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a Person's remark in the address book.
+ * Guarantees: immutable; is always valid
+ */
+public class Remark {
+    private final String value;
+
+    public Remark(String remark) {
+        requireNonNull(remark);
+        this.value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        // instanceof handles nulls
+        if (!(other instanceof Remark)) {
+            return false;
+        }
+        Remark otherRemark = (Remark) other;
+        return this.value.equals(otherRemark.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.value.hashCode();
+    }
+}
