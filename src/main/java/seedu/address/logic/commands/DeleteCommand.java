64bcab8513f679eab578b89ab341e4c103749840
aisyah ally <>
2025-03-12 15:43:31 +0800

    Edit error message

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -25,7 +25,7 @@ public class DeleteCommand extends Command {
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
 
-    public static final String MESSAGE_EMPTY_LIST = "No candidate to delete! The address book is empty.";
+    public static final String MESSAGE_EMPTY_LIST = "No candidate to delete! The displayed list is empty.";
 
     private final Index targetIndex;
 
