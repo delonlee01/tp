00c92266accb7e56ab56ff7761140550c41b20bf
delonlee01 <delonlee2001@hotmail.com>
2025-03-17 18:52:13 +0800

    Add comment field in Person

---
+++ src/main/java/seedu/recruittrackpro/model/person/Comment.java
@@ -0,0 +1,59 @@
+package seedu.recruittrackpro.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.recruittrackpro.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's comment in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidComment(String)}
+ */
+public class Comment {
+    public static final String MESSAGE_CONSTRAINTS = "Comments should not exceed 500 characters";
+
+    public static final String VALIDATION_REGEX = ".{0,500}";
+
+    public final String value;
+
+    /**
+     * Constructs a {@code Comment}.
+     *
+     * @param comment A valid comment.
+     */
+    public Comment(String comment) {
+        requireNonNull(comment);
+        checkArgument(isValidComment(comment), MESSAGE_CONSTRAINTS);
+        this.value = comment;
+    }
+
+    /**
+     * Returns true if a given string is a valid name.
+     */
+    public static boolean isValidComment(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Comment)) {
+            return false;
+        }
+
+        Comment otherComment = (Comment) other;
+        return value.equals(otherComment.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
--- src/main/java/seedu/recruittrackpro/model/person/Person.java
+++ src/main/java/seedu/recruittrackpro/model/person/Person.java
@@ -24,17 +24,19 @@ public class Person {
     // Data fields
     private final Address address;
     private final Set<Tag> tags = new HashSet<>();
+    private final Comment comment;
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, address, tags);
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Comment comment) {
+        requireAllNonNull(name, phone, email, address, tags, comment);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
         this.tags.addAll(tags);
+        this.comment = comment;
     }
 
     public Name getName() {
@@ -59,6 +61,10 @@ public class Person {
      */
     public Set<Tag> getTags() {
         return Collections.unmodifiableSet(tags);
+    }
+
+    public Comment getComment() {
+        return comment;
     }
 
     /**
@@ -94,13 +100,14 @@ public class Person {
                 && phone.equals(otherPerson.phone)
                 && email.equals(otherPerson.email)
                 && address.equals(otherPerson.address)
-                && tags.equals(otherPerson.tags);
+                && tags.equals(otherPerson.tags)
+                && comment.equals(otherPerson.comment);
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(name, phone, email, address, tags, comment);
     }
 
     @Override
@@ -111,6 +118,7 @@ public class Person {
                 .add("email", email)
                 .add("address", address)
                 .add("tags", tags)
+                .add("comment", comment)
                 .toString();
     }
 
