841d279259a3686a34311ce5ed9af840c46f98be
KarKuen <karkuenkakkoii@gmail.com>
2025-03-20 12:13:56 +0800

    Fix test code style

--- src/test/java/seedu/recruittrackpro/logic/predicates/PhoneContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/recruittrackpro/logic/predicates/PhoneContainsKeywordsPredicateTest.java
@@ -1,12 +1,15 @@
 package seedu.recruittrackpro.logic.predicates;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.recruittrackpro.testutil.PersonBuilder;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class PhoneContainsKeywordsPredicateTest {
     @Test
