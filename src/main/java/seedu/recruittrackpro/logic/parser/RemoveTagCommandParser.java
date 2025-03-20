7ed5f080f672d73a7c35a507003c92e56a33ad55
kylieluk88 <e0969639@u.nus.edu>
2025-03-20 12:35:24 +0800

    Edit JavaDoc comments

--- src/main/java/seedu/recruittrackpro/logic/parser/RemoveTagCommandParser.java
+++ src/main/java/seedu/recruittrackpro/logic/parser/RemoveTagCommandParser.java
@@ -10,12 +10,12 @@ import seedu.recruittrackpro.logic.parser.exceptions.ParseException;
 import seedu.recruittrackpro.model.tag.Tag;
 
 /**
- * Parses input arguments and creates a new DeleteCommand object
+ * Parses input arguments and creates a new RemoveTagCommand object
  */
 public class RemoveTagCommandParser implements Parser<RemoveTagCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
+     * Parses the given {@code String} of arguments in the context of the RemoveTagCommand
      * and returns a RemoveTagCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
