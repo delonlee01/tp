2b4a2e9f8c7b9cab30be26dd5b6ba94fe4d547c4
kylieluk88 <e0969639@u.nus.edu>
2025-03-25 21:45:13 +0800

    Edit errir message to catch empty from/ or to/ values

--- src/main/java/seedu/recruittrackpro/logic/parser/EditTagCommandParser.java
+++ src/main/java/seedu/recruittrackpro/logic/parser/EditTagCommandParser.java
@@ -17,17 +17,17 @@ public class EditTagCommandParser implements Parser<EditTagCommand> {
     public EditTagCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(
                 args, PREFIX_FROM, PREFIX_TO);
-        if (argMultimap.getValue(PREFIX_FROM).isEmpty() || argMultimap.getValue(PREFIX_TO).isEmpty()) {
+
+        String tagName = argMultimap.getValue(PREFIX_FROM).orElse("").trim();
+        String newTag = argMultimap.getValue(PREFIX_TO).orElse("").trim();
+
+        if (tagName.isEmpty() || newTag.isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditTagCommand.MESSAGE_USAGE));
         }
 
         try {
             String preamble = argMultimap.getPreamble().trim();
             Index index = ParserUtil.parseIndex(preamble);
-            String tagName = argMultimap.getValue(PREFIX_FROM).orElseThrow(() ->
-                    new ParseException(Tag.MESSAGE_CONSTRAINTS));
-            String newTag = argMultimap.getValue(PREFIX_TO).orElseThrow(() ->
-                    new ParseException(Tag.MESSAGE_CONSTRAINTS));
             return new EditTagCommand(index, new Tag(tagName), new Tag(newTag));
         } catch (ParseException pe) {
             throw new ParseException(
