7776b748f74b34e2d57c011d51bfb0e59702e69e
kylieluk88 <e0969639@u.nus.edu>
2025-04-01 18:09:31 +0800

    Remove unused import statements

--- src/main/java/seedu/recruittrackpro/logic/commands/RemoveTagsCommand.java
+++ src/main/java/seedu/recruittrackpro/logic/commands/RemoveTagsCommand.java
@@ -2,7 +2,6 @@ package seedu.recruittrackpro.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.recruittrackpro.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.recruittrackpro.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.List;
 
